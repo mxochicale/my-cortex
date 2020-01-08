63f7753f0553a043f31ea32b2f27182db94c670f
mxochicale <perez.xochicale@gmail.com>
2020-01-08 08:05:45 +0000

    polish cute_funtion.py

--- tex/figures/cute_function.py
+++ tex/figures/cute_function.py
@@ -3,17 +3,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Compute sin(1 / xy)
+# Compute sin(x)+sin(y)
 sz = 5000
-X = np.linspace(-1, 1, sz)
-Y = np.linspace(-1, 1, sz)
+X = np.linspace(-50, 50, sz)
+Y = np.linspace(-50, 50, sz)
 X, Y = np.meshgrid(X, Y)
 Z = np.sin(X) + np.sin(Y)
 
 # Plot it
 fig, ax = plt.subplots(1)
 im = ax.imshow(Z, cmap=plt.get_cmap("plasma"),
-               origin="lower", extent=(-1, 1, -1, 1))
+               origin="lower")
+		#, extent=(-1, 1, -1, 1))
 plt.colorbar(im, shrink=1, aspect=10)
 ax.set_xlabel("X", fontsize=16)
 ax.set_ylabel("Y", fontsize=16)
