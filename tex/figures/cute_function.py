de61600a20a190dc011e6d49f368dfa99fed6036
mxochicale <perez.xochicale@gmail.com>
2020-01-08 08:10:15 +0000

    limit axis to 10 for cute_function.py

--- tex/figures/cute_function.py
+++ tex/figures/cute_function.py
@@ -5,16 +5,15 @@ import numpy as np
 
 # Compute sin(x)+sin(y)
 sz = 5000
-X = np.linspace(-50, 50, sz)
-Y = np.linspace(-50, 50, sz)
+X = np.linspace(-10, 10, sz)
+Y = np.linspace(-10, 10, sz)
 X, Y = np.meshgrid(X, Y)
 Z = np.sin(X) + np.sin(Y)
 
 # Plot it
 fig, ax = plt.subplots(1)
 im = ax.imshow(Z, cmap=plt.get_cmap("plasma"),
-               origin="lower")
-		#, extent=(-1, 1, -1, 1))
+               origin="lower", extent=(-10, 10, -10, 10))
 plt.colorbar(im, shrink=1, aspect=10)
 ax.set_xlabel("X", fontsize=16)
 ax.set_ylabel("Y", fontsize=16)
